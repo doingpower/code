d6fa97c35b13d1888a68b6dafb6a85051edfb7d2
Zuoqiu-Yingyi <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2019-06-07 22:07:30 +0800

    更新

--- 主机与从机2/__1/__1.ino
+++ 主机与从机2/__1/__1.ino
@@ -1,52 +1,171 @@
 # include<IRremote.h>
+//11口3口有问题
+const int TrigPin = 12;//非PWM
+const int EchoPin = 10;//PWM
 
-const int TrigPin = 2;
-const int EchoPin = 3;
+int RECV_PIN = 9;//定义红外接收器引脚为9(PWM)
+
+const int AIN1 = 4;
+const int AIN2 = 2;
+const int PWMA = 5;
+const int PWMB = 6;
+const int BIN1 = 7;
+const int BIN2 = 8;
+
+int BV = 0, flag = 0, CCT = 0;
+int BV_R = 0, BV_B = 0;
 float cm = 15.0;
-int light_PIN = 8;//定义灯带引脚为8
-int RECV_PIN = 11;//定义红外接收器引脚为11
+int getup = 1;
+
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(TrigPin,OUTPUT);
-  pinMode(EchoPin,INPUT);
-  irrecv.enableIRIn();//使红外能接受
-  pinMode(light_PIN,OUTPUT);
-  // put your setup code here, to run once:
 
+  pinMode(TrigPin, OUTPUT);
+  pinMode(EchoPin, INPUT);
+
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(PWMA, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+
+  irrecv.enableIRIn();//使红外能接收
+
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(BIN1, HIGH);
+
+  digitalWrite(AIN2, LOW);
+  digitalWrite(BIN2, LOW);
 }
 
 void loop() {
-  if(irrecv.decode(&results))
+  light();
+
+  if (irrecv.decode(&results))
   {
-          digitalWrite(light_PIN,HIGH);
-      digitalWrite(TrigPin,LOW);//低高低电平发一个短时间脉冲去TrigPin
-      delayMicroseconds(2);
-      digitalWrite(TrigPin,HIGH);
-      delayMicroseconds(10);
-      digitalWrite(TrigPin,LOW);
-    
-      cm = pulseIn(EchoPin,HIGH)/58.0;//将回拨时间换算成cm
-      cm = (int(cm*100.0))/100.0;//保留两位小数
+    digitalWrite(AIN1, HIGH);
+    digitalWrite(AIN2, LOW);
+    digitalWrite(BIN1, HIGH);
+    digitalWrite(BIN2, LOW);
+    digitalWrite(TrigPin, LOW); //低高低电平发一个短时间脉冲去TrigPin
+    delayMicroseconds(2);
+    digitalWrite(TrigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TrigPin, LOW);
+
+    cm = pulseIn(EchoPin, HIGH) / 58.0; //将回拨时间换算成cm
+    cm = (int(cm * 100.0)) / 100.0; //保留两位小数
   }//关灯后才开始测距
-    if(results.value==0x123456||cm>30)
-    {
+  if (results.value == 0x123456 || (cm > 30 && getup == 1))
+  {
     /*Serial.println(results.value,HEX);//以16进制换行输出接收代码
-    Serial.println();//为了便于观看加一个空行*/
-        digitalWrite(light_PIN,HIGH);
-    }
-  //关灯上床时因检测不到人但是还未躺下半夜有人回寝室情况灯带亮
- else
+      Serial.println();//为了便于观看加一个空行*/
+    digitalWrite(AIN1, HIGH);
+    digitalWrite(AIN2, LOW);
+    digitalWrite(BIN1, HIGH);
+    digitalWrite(BIN2, LOW);
+    digitalWrite(TrigPin, LOW); //低高低电平发一个短时间脉冲去TrigPin
+    delayMicroseconds(2);
+    digitalWrite(TrigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TrigPin, LOW);
+    cm = pulseIn(EchoPin, HIGH) / 58.0; //将回拨时间换算成cm
+    cm = (int(cm * 100.0)) / 100.0; //保留两位小数
+
+  }
+  //关灯上床时因检测不到人但是还未躺下半夜有人回寝室情况灯带亮当人躺下时记录最后一次cm值
+  else
   {
-    digitalWrite(light_PIN,LOW);
+    digitalWrite(AIN1, LOW);
+    digitalWrite(AIN2, LOW);
+    digitalWrite(BIN1, LOW);
+    digitalWrite(BIN2, LOW);
   }//未关灯或都躺下且无外来人时灯带熄灭
-   if(cm>30&&(!irrecv.decode(&results)))
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(BIN1, HIGH);
+  digitalWrite(TrigPin, LOW); //低高低电平发一个短时间脉冲去TrigPin
+  delayMicroseconds(2);
+  digitalWrite(TrigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TrigPin, LOW);
+
+  cm = pulseIn(EchoPin, HIGH) / 58.0; //将回拨时间换算成cm
+  cm = (int(cm * 100.0)) / 100.0; //保留两位小数
+  //人躺下后时刻更新cm值
+  if (cm > 30 && (!irrecv.decode(&results)) && getup == 1)
   {
-    digitalWrite(light_PIN,HIGH);
+    digitalWrite(AIN1, HIGH);
+    digitalWrite(AIN2, LOW);
+    digitalWrite(BIN1, HIGH);
+    digitalWrite(BIN2, LOW);
+    getup = 0;
   }//起夜时灯带亮
-      irrecv.resume();
+  if ((getup == 0) && !irrecv.decode(&results))
+  {
+    digitalWrite(AIN1, LOW);
+    digitalWrite(AIN2, LOW);
+    digitalWrite(BIN1, LOW);
+    digitalWrite(BIN2, LOW);    //起夜进入阳台后灯带熄灭
+    if (irrecv.decode(&results))
+      digitalWrite(AIN1, HIGH);
+    digitalWrite(AIN2, LOW);
+    digitalWrite(BIN1, HIGH);
+    digitalWrite(BIN2, LOW);//从阳台出来后灯亮
+    getup = 1;
+  }
+
+  irrecv.resume();
   // put your main code here, to run repeatedly:
+
+}
+void light()
+{
+  String inString = "";
+  while (Serial.available() > 0) {
+    int inChar = Serial.read();
+    inString += (char)inChar;
+    delay(24);
+  }
+  if (inString != "") {
+    flag = inString[0] - '0';
+    BV = inString[1] - '0';
+    CCT = inString[2] - '0';
+  }
+
+  BV_set(CCT);
+  if (flag) {
+
+    analogWrite(PWMA, BV_B);
+    analogWrite(PWMB, BV_R);
+  }
+  else {
+    analogWrite(PWMA, 0);
+    analogWrite(PWMB, 0);
+  }
+
+
+}
+
+void BV_set(int cct)
+{
+  BV_R = BV_B = BV * 10;
+  if (cct < 5) {//变冷
+    cct = 5 - cct;
+
+    BV_B += 2 * BV * cct;
+    BV_R -= 2 * BV * cct;
+
+  }
+  else {
+    cct -= 4;
+
+    BV_B -= 2 * BV * cct;
+    BV_R += 2 * BV * cct;
+
+  }
 
 }
